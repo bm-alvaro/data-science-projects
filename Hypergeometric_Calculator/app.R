00835d32948ebe6d12d2662655888cc22a1954fa
bm-alvaro <85340857+bm-alvaro@users.noreply.github.com>
2023-11-20 09:58:35 +0100

    Building an Hypergeometric calculator

---
+++ Hypergeometric_Calculator/app.R
@@ -0,0 +1,63 @@
+library(shiny)
+library(shinythemes)
+
+ui <- fluidPage(theme = shinytheme("sandstone"),
+  titlePanel("Hypergeometric Calculator"),
+  sidebarLayout(
+    sidebarPanel(
+      numericInput("deck_size", "Deck Size:", value = 60),
+      numericInput("num_copies_interest", "Number of Copies of Card:", value = 4),
+      numericInput("cards_drawn", "Cards Drawn:", value = 7),
+      numericInput("successes_wanted", "Successes Wanted:", value = 1),
+      actionButton("calculate", "Calculate"),
+      br(),
+      br(),
+      textOutput("result")
+    ),
+    mainPanel(
+      br(),
+      tableOutput("history_table")
+    )
+  )
+)
+
+server <- function(input, output) {
+  history <- reactiveValues(data = data.frame(
+    Deck_Size = numeric(),
+    Num_Copies_Interest = numeric(),
+    Cards_Drawn = numeric(),
+    Successes_Wanted = numeric(),
+    Probability = numeric()
+  ))
+  
+  observeEvent(input$calculate, {
+    prob <- dhyper(input$successes_wanted, input$num_copies_interest, 
+                   input$deck_size - input$num_copies_interest, 
+                   input$cards_drawn)
+    
+    new_row <- data.frame(
+      Deck_Size = input$deck_size,
+      Num_Copies_Interest = input$num_copies_interest,
+      Cards_Drawn = input$cards_drawn,
+      Successes_Wanted = input$successes_wanted,
+      Probability = prob
+    )
+    
+    history$data <- rbind(history$data, new_row)
+  })
+  
+  output$history_table <- renderTable({
+    history$data
+  })
+  
+  output$result <- renderText({
+    prob <- dhyper(input$successes_wanted, input$num_copies_interest, 
+                   input$deck_size - input$num_copies_interest, 
+                   input$cards_drawn)
+    
+    paste("Probability of getting exactly", input$successes_wanted, 
+          "of the card:", prob)
+  })
+}
+
+shinyApp(ui = ui, server = server)
